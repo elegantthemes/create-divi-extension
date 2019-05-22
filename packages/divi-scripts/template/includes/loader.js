bce7cf79086f5c1d7994ce4fbfa6158d4d1bf1dc
bitfade <bitfade@bitfade.com>
2019-05-22 07:37:42 +0200

    DA2 :: Allow 3rd party code to import Divi components.
    This PR allows us to expose components via webpack's library export.
    
    So far, only the `API` object has been included in the library and
    it can be used like this:
    
    ```JS
    import API from '@divi/API';
    API.registerModules(modules);
    ```
    
    As opposed as the old method which required 'jQuery' and a window event:
    
    ```JS
    import $ from 'jquery';
    $(window).on('et_builder_api_ready', (event, API) => {
      API.registerModules(modules);
    });
    ```
    
    PR is backward compatible so the above code still works without
    changes.
    
    Additional components can be very easily exported in `app.jsx`
    
    ```JS
    const components = {
      ETBuilderPreloader,
      controls: {
        ETBuilderControlInput,
      },
    };
    ```
    
    And imported in 3P code:
    
    ```JS
    // 3P code willing to include our components
    import { ETBuilderControlInput } from '@divi/components/controls';
    ```
    
    The following changes were required:
    1. `ETBuilderAPI` is instantiated upon exporting rather than when
    builder initializes.
    2. Extension bundles are enqueued after builder bundle (so the
    exports are available to them).
    3. Webpack config changes in both builder and `divi-create-extension`
    
    Fixes https://github.com/elegantthemes/divi/issues/14636

--- packages/divi-scripts/template/includes/loader.js
+++ packages/divi-scripts/template/includes/loader.js
@@ -1,9 +1,7 @@
 // External Dependencies
-import $ from 'jquery';
+import API from '@divi/API';
 
 // Internal Dependencies
 import modules from './modules';
 
-$(window).on('et_builder_api_ready', (event, API) => {
-  API.registerModules(modules);
-});
+API.registerModules(modules);
