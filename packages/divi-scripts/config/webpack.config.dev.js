84c215255345f8b8361e46e85ef02eae6447ea58
Lanre Smith <abims212@gmail.com>
2020-11-03 13:49:53 +0100

    fix build failing if project has the word "fields" in its absolute path

--- packages/divi-scripts/config/webpack.config.dev.js
+++ packages/divi-scripts/config/webpack.config.dev.js
@@ -291,7 +291,7 @@ module.exports = {
           // By default we support CSS Modules with the extension .module.css
           {
             test: /\.(s?css|sass)$/,
-            exclude: [/\.module\.css$/, /fields/],
+            exclude: [/\.module\.css$/, /includes\/fields/],
             use: [
               require.resolve('style-loader'),
               {
